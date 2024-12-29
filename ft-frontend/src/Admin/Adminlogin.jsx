4fda86c921db2b5046866c63a7913541d5b1d125
Salah-XD <hudeensala948@gmail.com>
2024-12-29 15:26:45 +0530

    test:apiurl

--- ft-frontend/src/Admin/Adminlogin.jsx
+++ ft-frontend/src/Admin/Adminlogin.jsx
@@ -34,7 +34,7 @@ const Adminlogin = () => {
 
     try {
       const response = await axios.post(
-        `${process.env.API_URL}/api/admin/login`,
+        `https://ftbackend.vercel.app/api/admin/login`,
         credentials,
         { headers }
       );
